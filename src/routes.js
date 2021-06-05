8a379fef9cbcd6dd1945cf1d105820e69fbe8191
Renan Muniz <fmrenan@outlook.com>
2021-06-05 13:23:52 -0300

    User Routes

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const UserController = require("./controllers/UserController");
+
+const routes = express.Router();
+
+//User resources
+routes.get("/users", UserController.findAll); 
+routes.get("/users/:user_id", UserController.findById); 
+routes.post("/users", UserController.insert); 
+routes.put("/users/:user_id", UserController.update); 
+
+module.exports = routes; 
